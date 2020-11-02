aabc3f34b89e74975c7f48db958367c76d394f4e
ervitis <vitomarti@gmail.com>
2020-11-02 21:24:34 +0100

    chore: new form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { TitleComponent } from './atoms/title/title.component';
+import { FormsComponent } from './forms/forms.component';
+import { DetailComponent } from './forms/detail/detail.component';
+import { MainPictureComponent } from './atoms/main-picture/main-picture.component';
+import { NewComponent } from './forms/new/new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    TitleComponent,
+    FormsComponent,
+    DetailComponent,
+    MainPictureComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/atoms/main-picture/main-picture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-picture',
+  templateUrl: './main-picture.component.html',
+  styleUrls: ['./main-picture.component.css']
+})
+export class MainPictureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
